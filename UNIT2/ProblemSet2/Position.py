d6ebce78a7431debd2a73bd67d156678db58bbfa
John David Ibañez <JhonhRge@gmail.com>
2018-06-23 21:48:51 -0500

    Update and rename ps2.py to Position.py

---
+++ UNIT2/ProblemSet2/Position.py
@@ -0,0 +1,64 @@
+# 6.00.2x Problem Set 2: Simulating robots
+
+import math
+import random
+
+#import ps2_visualize
+import pylab
+import ps2_visualize
+##################
+## Comment/uncomment the relevant lines, depending on which version of Python you have
+##################
+
+# For Python 3.5:
+#from ps2_verify_movement35 import testRobotMovement
+# If you get a "Bad magic number" ImportError, you are not using Python 3.5 
+
+# For Python 3.6:
+from ps2_verify_movement36 import testRobotMovement
+# If you get a "Bad magic number" ImportError, you are not using Python 3.6
+
+
+# === Provided class Position
+class Position(object):
+    """
+    A Position represents a location in a two-dimensional room.
+    """
+    def __init__(self, x, y):
+        """
+        Initializes a position with coordinates (x, y).
+        """
+        self.x = x
+        self.y = y
+        
+    def getX(self):
+        return self.x
+    
+    def getY(self):
+        return self.y
+    
+    def getNewPosition(self, angle, speed):
+        """
+        Computes and returns the new Position after a single clock-tick has
+        passed, with this object as the current position, and with the
+        specified angle and speed.
+
+        Does NOT test whether the returned position fits inside the room.
+
+        angle: number representing angle in degrees, 0 <= angle < 360
+        speed: positive float representing speed
+
+        Returns: a Position object representing the new position.
+        """
+        old_x, old_y = self.getX(), self.getY()
+        angle = float(angle)
+        # Compute the change in position
+        delta_y = speed * math.cos(math.radians(angle))
+        delta_x = speed * math.sin(math.radians(angle))
+        # Add that to the existing position
+        new_x = old_x + delta_x
+        new_y = old_y + delta_y
+        return Position(new_x, new_y)
+
+    def __str__(self):  
+        return "(%0.2f, %0.2f)" % (self.x, self.y)
